a631934d12c6ad823a32cbe01d5c630f5ddb9b5f
Sanjay SP <spsanjay6567@gmail.com>
2024-06-01 17:28:50 +0530

    Initial Setup and Navbar Implemented

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <img
+        className={styles.menuBtn}
+        src={
+          menuOpen ? "/assets/nav/closeIcon.png" : "/assets/nav/menuIcon.png"
+        }
+        alt="menu-icon"
+        onClick={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      ></img>
+      <div className={styles.menu}>
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
