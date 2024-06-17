456e8d2494ad132bbade2e1c21a6b97c7887aec1
Sanjay SP <spsanjay6567@gmail.com>
2024-06-17 15:13:07 +0530

    Implemented "Projects' section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import styles from './App.module.css';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import Experience from './components/Experience/Experience';
+import React from "react";
+import styles from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Experience/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
     </div>
   );
 }
