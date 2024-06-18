996bf4b83afdad3dab37b91ed09739819ace6a22
Sanjay SP <spsanjay6567@gmail.com>
2024-06-18 19:42:59 +0530

    build failed error resolved

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
-import { Projects } from "./components/Projects/Projects";
+// import { Projects } from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 
 function App() {
