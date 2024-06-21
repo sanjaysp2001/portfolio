296f9427ac9a135d47772f9535347639a8d877fb
Sanjay SP <spsanjay6567@gmail.com>
2024-06-21 14:44:43 +0530

    Implemented Certifications

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 // import { Projects } from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import { Certifications } from "./components/Certifications/Certifications";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Experience />
       {/* <Projects /> */}
+      <Certifications/>
       <Contact/>
     </div>
   );
