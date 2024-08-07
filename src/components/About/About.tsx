5959ca81fc910d3b5c4f90d98b35f7312c487d21
Sanjay S P <59553361+sanjaysp2001@users.noreply.github.com>
2024-08-07 14:20:39 +0530

    Updated Skills

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,7 +2,7 @@ import styles from "./About.module.css";
 import assets from "../../data/assets.json";
 import { motion } from "framer-motion";
 import { useState } from "react";
-
+import about from "../../data/about.json";
 const About = () => {
   const [currentImage, setCurrentImage] = useState(assets.about.aboutImage1);
   return (
@@ -29,16 +29,7 @@ const About = () => {
 
           <div className={styles.aboutItem}>
             <h3>Full Stack Developer</h3>
-            <p>
-              A passionate and dedicated full-stack developer specializing in
-              the MERN (MongoDB, Express.js, React, Node.js) stack. With a
-              strong foundation in both front-end and back-end technologies, I
-              create robust, scalable, and user-friendly web applications. My
-              expertise lies in designing responsive user interfaces, developing
-              efficient server-side logic, and managing databases seamlessly.
-              Explore my portfolio to see how I bring ideas to life through
-              clean code and innovative solutions.
-            </p>
+            <p>{about.description}</p>
           </div>
         </div>
       </div>
